b617e3b3b8926354a5f35d53e00aef04312b8e42
Parth Sane <pvs3325@rit.edu>
2019-06-05 17:48:02 -0400

    Further JS location fixes

--- js/checkboxredirectlogic.js
+++ js/checkboxredirectlogic.js
@@ -1,6 +1,6 @@
 function set_checked(checked,item) {
     $('input[name='+item+']').attr('checked', checked);
     if($('.box:checked').length == $('.box').length){
-        window.location.href = "https://boggartfly.github.io/wiggly-random-moving-button/";
+        window.location.href = "/index.html";
     }
 }
