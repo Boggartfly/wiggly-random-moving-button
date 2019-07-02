c1f6687346f012755222a506f7b2273fed561839
Parth Sane <laerdevstudios@gmail.com>
2019-07-02 13:23:14 -0400

    Added a lot of content for finalization

--- js/cat.js
+++ js/cat.js
@@ -1,17 +1,13 @@
 function iscat(){
     $('#result').html('<p>It\'s a cat!</p> \
     <br><a href="blindness-instruction.html">\
-    Next, try with blindness simulation and without accessibility features</a>');
+    Next</a>');
 }
 function notcat(){
     $('#result').html('<p>It\'s not a cat!</p>'); 
 }
 function iscatblind(){
-    $('#result').html('<p>It\'s a cat!</p><br>');
-}
-function redirectToAccessible(){
-    window.location.href = "./blindness-accessible.html";
-}
-function redirectFirst(){
-    window.location.href = "./blindness-instruction.html";
+    $('#result').html('<p>It\'s a cat!</p><br>\
+    <a href="conclusion.html">\
+    Next</a>');
 }
--- js/redirect.js
+++ js/redirect.js
@@ -1,3 +1,18 @@
-setTimeout(function() {
+function redirectInaccessible(){
+    window.location.href = "./blindness-inaccessible.html";
+}
+function redirectToAccessible(){
     window.location.href = "./blindness-accessible.html";
-  }, 4000);
+}
+function redirectToInstructions(){
+    window.location.href = "./blindness-instruction.html";
+}
+function redirectToHome(){
+    window.location.href = "./index.html";
+}
+function redirectToConclusion(){
+    window.location.href = "./conclusion.html";
+}
+function redirectToAccessibleInstruction(){
+    window.location.href = "./accessible-instruction.html";
+}
