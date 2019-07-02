ff636e58ab06db2195f557892601387b4684567f
Parth Sane <laerdevstudios@gmail.com>
2019-07-02 11:45:03 -0400

    Added new files with respect to content

--- js/cat.js
+++ js/cat.js
@@ -1,11 +1,14 @@
 function iscat(){
     $('#result').html('<p>It\'s a cat!</p> \
-    <br><a href="blindness-accessible.html">\
-    Next, try with blindness simulation and accessibility features</a>');
+    <br><a href="blindness-inaccessible.html">\
+    Next, try with blindness simulation and without accessibility features</a>');
 }
 function notcat(){
     $('#result').html('<p>It\'s not a cat!</p>'); 
 }
 function iscatblind(){
     $('#result').html('<p>It\'s a cat!</p><br>');
+}
+function redirectToAccessible(){
+    window.location.href = "./blindness-accessible.html";
 }
